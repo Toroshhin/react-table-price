b78439367861827fff2816d34560e148ec8d1bbb
toroshhin <sergeitoroshin@yandex.ru>
2021-03-02 22:41:10 +0300

    commented out bugs

--- src/components/InputPrice.jsx
+++ src/components/InputPrice.jsx
@@ -7,33 +7,51 @@ class InputPrice extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: true,
+      active: false,
       value: ''
-    };
-    const { keys } = this.props;
+    }
+    const { keys } = this.props
+    // this.textInput = React.createRef()
+    // this.focusTextInput = this.focusTextInput.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
   }
 
-  onStorage = () => (
+  onStorage = () => {
     localStorage.setItem(this.props.keys, this.state.value)
-  )
+  }
+
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.onStorage();
+    }
+  }
+
+
+
+
+  // focusTextInput() {
+  //   this.textInput.current.focus()
+  //   this.setState({ active: false })
+  // }
 
   render() {
     return (
-      <>
+      <div /*onClick={this.focusTextInput}*/>
         <Input
           style={{
             cursor: 'pointer'
           }}
           type={'number'}
-          onChange={(e) => { this.setState({ value: e.currentTarget.value }) }}
+          onChange={(e) => { this.setState({ value: e.currentTarget.value }); this.onStorage(); }}
+          onKeyPress={this.handleKeyPress}
           onBlur={() => { this.onStorage(); this.setState({ active: true }) }}
-          // onDoubleClick={() => { this.setState({ active: false }); console.log('Двойной клик'); }}
-          onClick={() => { this.setState({ active: false }); console.log('Клик'); }}
           transparent
           disabled={this.state.active}
+          // ref={this.textInput}
           key={this.props.keys}
           defaultValue={localStorage.getItem(this.props.keys)} />
-      </>
+      </div >
 
     );
   }
